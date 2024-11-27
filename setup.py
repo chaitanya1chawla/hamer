cafef970944ce379362351eea73ef242d99adeb2
Chaitanya Chawla <chaitanya1chawlla@gmail.com>
2024-11-27 13:39:11 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,17 +8,20 @@ setup(
     install_requires=[
         'gdown',
         'numpy',
-        'opencv-python',
+        # 'opencv-python',
         'pyrender',
         'pytorch-lightning',
-        'scikit-image',
+        # 'scikit-image',
         'smplx==0.1.28',
-        'torch',
-        'torchvision',
+        # 'torch',
+        # 'torchvision',
         'yacs',
         
         'chumpy @ git+https://github.com/mattloper/chumpy',
         'mmcv==1.3.9',
+        'mmpose==0.29.0',
+        # 'torch==2.1.0', # this can change depending on your cuda version [but in general, local cu12x works with other virtual env cu12y]
+        # 'torch==2.1.0+cu121', # this can change depending on your cuda version [but in general, local cu12x works with other virtual env cu12y]
         'timm',
         'einops',
         'xtcocotools',
